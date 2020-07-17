796cf2abd14a8197947e727bfc62f4fdc454a39d
Anish kr singh <65110035+Ankrsi@users.noreply.github.com>
2020-07-17 22:04:27 +0530

    Add files via upload

---
+++ whatsapp.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+import time
+import winsound
+on="offline"
+off=None
+i=0
+web=webdriver.Chrome("D:/chromedriver.exe")
+web.get("https://web.whatsapp.com/")
+time.sleep(20)
+f1=web.find_element_by_xpath('//*[@id="pane-side"]/div[1]/div/div/div[16]/div/div/div[2]/div[1]/div[1]/span').click()
+#f1=web.find_element_by_xpath('//*[@id="pane-side"]/div[1]/div/div/div[18]/div/div/div[2]/div[1]/div[1]/span').click()
+time.sleep(3)
+while(True):
+    f2=web.find_element_by_xpath('//*[@id="main"]/header/div[2]/div[2]').text
+    if(f2=="online"):
+        on="Online"
+        print(on)
+        while(i<1):
+            winsound.Beep(1000,300)
+            i+=1
+    else:
+        on=f2
+        print(on)
+        i=0
+
